dfcb73b2e91f8d375dd9853548d9dad53d40a71a
suyh <su_787910081@sina.com>
2026-08-19 23:33:25 +0800

    mvc 扩展

---
+++ medcase-admin/src/main/java/com/ruoyi/mvc/constants/enums/BaseWebErrorCodeEnums.java
@@ -0,0 +1,36 @@
+package com.ruoyi.mvc.constants.enums;
+
+import com.ruoyi.mvc.error.IErrorCode;
+
+/**
+ * 错误码枚举
+ *
+ * @author suyh
+ * @since 2025-05-16
+ */
+public enum BaseWebErrorCodeEnums implements IErrorCode {
+    ACCESS_DENIED(1000403, "禁止访问"),
+    NO_IMPLEMENT(1000404, "功能代码还未实现"),
+    SERVICE_ERROR(1000500, "服务错误"),
+    SYSTEM_UNSUPPORTED(1000601, "Not Supported: {0}"),
+
+    ;
+
+    private final int code;
+    private final String msg;
+
+    BaseWebErrorCodeEnums(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+}
