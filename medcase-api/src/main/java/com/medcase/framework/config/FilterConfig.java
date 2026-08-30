358f73c8ba41dda2e8f9043c8ab4e62f9baf1229
suyh <su_787910081@sina.com>
2026-08-30 22:17:05 +0800

    代码整理

--- medcase-api/src/main/java/com/medcase/framework/config/FilterConfig.java
+++ medcase-api/src/main/java/com/medcase/framework/config/FilterConfig.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.medcase.common.constant.Constants;
-import com.medcase.common.filter.RefererFilter;
 import com.medcase.common.filter.RepeatableFilter;
 import com.medcase.common.filter.XssFilter;
 import com.medcase.common.utils.StringUtils;
@@ -27,9 +25,6 @@ public class FilterConfig {
     @Value("${xss.urlPatterns}")
     private String urlPatterns;
 
-    @Value("${referer.allowed-domains}")
-    private String allowedDomains;
-
     @SuppressWarnings({ "rawtypes", "unchecked" })
     @Bean
     @ConditionalOnProperty(value = "xss.enabled", havingValue = "true")
@@ -43,23 +38,6 @@ public class FilterConfig {
         registration.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
         Map<String, String> initParameters = new HashMap<String, String>();
         initParameters.put("excludes", excludes);
-        registration.setInitParameters(initParameters);
-        return registration;
-    }
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    @Bean
-    @ConditionalOnProperty(value = "referer.enabled", havingValue = "true")
-    public FilterRegistrationBean refererFilterRegistration() {
-
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setDispatcherTypes(DispatcherType.REQUEST);
-        registration.setFilter(new RefererFilter());
-        registration.addUrlPatterns(Constants.RESOURCE_PREFIX + "/*");
-        registration.setName("refererFilter");
-        registration.setOrder(FilterRegistrationBean.HIGHEST_PRECEDENCE);
-        Map<String, String> initParameters = new HashMap<String, String>();
-        initParameters.put("allowedDomains", allowedDomains);
         registration.setInitParameters(initParameters);
         return registration;
     }
